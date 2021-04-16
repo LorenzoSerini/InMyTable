b8625612f56453b9e58add76079c94c5b29abb51
alicegirolamini <alice.girolamini@studenti.unicam.it>
2021-04-16 11:28:18 +0200

    prova commit

--- src/main/java/com/inMyTable/InMyTable.java
+++ src/main/java/com/inMyTable/InMyTable.java
@@ -4,7 +4,7 @@ import com.inMyTable.view.View;
 import com.inMyTable.view.spring.SpringView;
 
 public class InMyTable {
-
+//commento
 	public static void main(String[] args) {
 		View view = new SpringView();
 		view.start();
