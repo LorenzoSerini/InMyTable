1ec4dcb9a8e7a5463358833fc3a9c63423c7ffa9
LorenzoSerini <lorenzose.1995@gmail.com>
2021-05-22 14:00:16 +0200

    changed database

--- src/main/java/it/unicam/cs/gp/inmytable/persistence/DBConnection.java
+++ src/main/java/it/unicam/cs/gp/inmytable/persistence/DBConnection.java
@@ -6,12 +6,9 @@ import org.apache.tomcat.jni.Local;
 import java.sql.*;
 
 public abstract class DBConnection {
-    private static final String CONNECTION_STRING = "jdbc:mysql://sql4.freesqldatabase.com:3306/sql4409749";
-    private static final String USERNAME = "sql4409749";
-    private static final String PASSWORD =  "Ta6C6ZdGjq";
-    private static final String AZURE_CONNECTION_STRING = "jdbc:sqlserver://inmytable.database.windows.net:1433";
-    private static final String AZURE_USERNAME = "group_project@inmytable";
-    private static final String AZURE_PASSWORD =  "InMyTable2021";
+    private static final String CONNECTION_STRING = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11414097";
+    private static final String USERNAME = "sql11414097";
+    private static final String PASSWORD =  "9JRJiUcdeg";
     private static final String LOCAL_CONNECTION_STRING = "jdbc:mysql://127.0.0.1:3306/inmytable";
     private static final String LOCAL_USERNAME = "root";
     private static final String LOCAL_PASSWORD =  "root";
@@ -33,7 +30,7 @@ public abstract class DBConnection {
     }
 
     private void defaultConnection() throws SQLException {
-        this.connection(LOCAL_CONNECTION_STRING, LOCAL_USERNAME, LOCAL_PASSWORD);
+        this.connection(CONNECTION_STRING, USERNAME, PASSWORD);
     }
 
     public Connection getConnection(){
